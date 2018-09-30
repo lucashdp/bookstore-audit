bbf2ef6de0f1e79633cd3601741c54bd3beac169
Lucas Duarte <lucashdp@outlook.com>
2018-09-30 11:19:07 -0300

    feat: implements the mongodb connection
    I implemented the mongo connection using a remote mongo
    database. I refaced the routes too.

--- app/routes/audit_routes.js
+++ app/routes/audit_routes.js
@@ -1,10 +1,43 @@
 const requestIp = require('request-ip');
 // routes/audit_routes.js
 module.exports = function (app, db) {
-    app.get('/audit', (req, res) => {
-        // You'll create your note here.
+    //----------GET----------------------------//
+    app.get('/v1/public/audits', (req, res) => {
+        db.collection('audits')
+            .find()
+            .toArray((err, items) => {
+                if (err) {
+                    res.send({ 'error': 'An error has occurred' });
+                } else {
+                    res.send(items);
+                }
+            });
+    });
+    app.get('/v1/public/audits/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('audits').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    //----------POST---------------------------//
+    app.post('/v1/public/audits', (req, res) => {
         const clientIp = requestIp.getClientIp(req);
-        console.log(clientIp);
-        res.send('Getting audits')
+        const audit = { text: req.body.body, title: req.body.title };
+        audit.clientIp = clientIp;
+        audit.timeAudit = new Date();
+
+        db.collection('audits').insert(audit, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
     });
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,5 +2,4 @@
 const auditRoutes = require('./audit_routes');
 module.exports = function (app, db) {
     auditRoutes(app, db);
-    // Other route groups could go here, in the future
 };
